d8e3072ddd0a6b610d1b00533922870ad778d317
Hoyt Hicks <hoytdawson@gmail.com>
2022-06-30 00:16:50 -0400

    Added horizontal collider proof of concept. Also fixed if statements in tire.cs to use the boolean of ray casting

--- Unity/RenaissanceCodersExample/Assets/Scripts/Tire.cs
+++ Unity/RenaissanceCodersExample/Assets/Scripts/Tire.cs
@@ -16,28 +16,60 @@ public class Tire : MonoBehaviour
     {
         hubNormalVec = -hubTransform.right;
         rdot = wheelRigidBody.velocity.y;
-        Physics.Raycast(hubTransform.position, hubNormalVec, out RaycastHit hitinfo, 20f);
-        if (hitinfo.distance < r0) {
+        
+
+
+        newRot = Quaternion.AngleAxis(-90, -hubTransform.up) * hubNormalVec;
+
+
+
+        if (Physics.Raycast(hubTransform.position, hubNormalVec, out RaycastHit hitinfo, 20f)) {
             r = hitinfo.distance;
-            Vector3 normalForce = new Vector3((float)(k * (r - r0) + c * rdot), 0f, 0f);
-            wheelRigidBody.AddRelativeForce(normalForce);
+            if (r< r0)
+            {
+                Vector3 normalForce = new Vector3((float)(k * (r - r0) + c * rdot), 0f, 0f);
+                wheelRigidBody.AddRelativeForce(normalForce);
+            }
+
         }
-        else {
-            r = 0;
+        else
+        {
             Vector3 normalForce = new Vector3(0f, 0f, 0f);
             wheelRigidBody.AddRelativeForce(normalForce);
         }
-        Debug.DrawRay(hubTransform.position, hubNormalVec, Color.red, 100f);
+
+        if (Physics.Raycast(hubTransform.position, newRot, out RaycastHit hitinfof, 5f))
+        {
+            r = hitinfof.distance;
+            if (r < r0)
+            {
+                r = hitinfof.distance;
+                Vector3 normalForce2 = -newRot * (float)(k * (r - r0) + c * rdot);
+                wheelRigidBody.AddRelativeForce(normalForce2);
+            }
+        }
+
+        else
+        {
+            Vector3 normalForce = new Vector3(0f, 0f, 0f);
+            wheelRigidBody.AddRelativeForce(normalForce);
+
+        }
+
+        Debug.DrawRay(hubTransform.position, hubNormalVec, Color.red, .1f);
         Debug.Log(hitinfo.distance);
+        Debug.DrawRay(hubTransform.position, newRot*.5f, Color.blue, .1f);
+        Debug.Log(hitinfof.distance);
 
     }
-  
+
     private double r0 = .5;
     public double k = 10;
     public double c = -.1;
     public double r;
     public double rdot;
     public Vector3 hubNormalVec;
+    public Vector3 newRot;
     private Transform hubTransform;
     private Rigidbody wheelRigidBody;
 
