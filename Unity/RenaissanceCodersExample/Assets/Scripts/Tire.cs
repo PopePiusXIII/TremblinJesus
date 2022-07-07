0d1ae99299bc4ac0da7fd480e2cee7595a640a5c
DESKTOP-J78GKAS\harley <harleyhicksgto@gmail.com>
2022-07-07 15:05:52 -0400

    wheel can bounce and is calculating slip ratio correctly while floating above the ground

--- Unity/RenaissanceCodersExample/Assets/Scripts/Tire.cs
+++ Unity/RenaissanceCodersExample/Assets/Scripts/Tire.cs
@@ -7,74 +7,92 @@ public class Tire : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Time.fixedDeltaTime = .001f;
         hubTransform = GetComponent<Transform>();
         wheelRigidBody = GetComponent<Rigidbody>();
         wheelRotateBody = hubTransform.Find("Visual").gameObject.GetComponent<Rigidbody>();
         wheelRotateTransform = hubTransform.Find("Visual").gameObject.GetComponent<Transform>();
-        wheelRotateBody.angularVelocity = -wheelRotateTransform.up * wheelRigidBody.velocity.z / r0;
+        wheelRotateBody.maxAngularVelocity = 10000f;
+        wheelRotateBody.angularVelocity = new Vector3(0f, 0f, -2f);
         wheelRotateTransform.localPosition = new Vector3(0f,0f,0f);
+        wheelRigidBody.velocity = hubTransform.right * 1;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        hubNormalVec = -hubTransform.right;
-        rdot = wheelRigidBody.velocity.y;
+        hubNormalVec = -hubTransform.forward;   //this is dumb notation need to figure out how to fix
+        rdot = hubTransform.InverseTransformDirection(wheelRigidBody.velocity).z;
 
-        if (Physics.Raycast(hubTransform.position, hubNormalVec, out RaycastHit hitinfo, 20f)) {
+
+        if (Physics.Raycast(hubTransform.position, hubNormalVec, out RaycastHit hitinfo, 5 * r0))
+        {
             r = hitinfo.distance;
-            if (r< r0)
-            {
-                Vector3 normalForce = new Vector3((float)(k * (r - r0) + c * rdot), 0f, 0f);
-                wheelRigidBody.AddRelativeForce(normalForce);
-            }
-
-        }
-        else
-        {
-            Vector3 normalForce = new Vector3(0f, 0f, 0f);
-            wheelRigidBody.AddRelativeForce(normalForce);
-        }
-
-        if (Physics.Raycast(hubTransform.position, hubTransform.forward, out RaycastHit hitinfof, 5f))
-        {
-            r = hitinfof.distance;
             if (r < r0)
             {
-                r = hitinfof.distance;
-                Vector3 normalForce2 = -hubTransform.forward * (float)(k * (r - r0) + c * rdot);
-                wheelRigidBody.AddRelativeForce(normalForce2);
+                normalForce = (k * (r - r0) + c * rdot);
+                longitdudinalForce = LongitudinalForce(wheelRigidBody, hitinfo);
+            }
+
+            else
+            {
+                r = r0;
+                normalForce = 0;
+                longitdudinalForce = LongitudinalForce(wheelRigidBody, hitinfo);
+                longitdudinalForce = 0;
             }
         }
 
+        wheelRigidBody.AddRelativeForce(new Vector3(longitdudinalForce, 0, normalForce));
+        wheelRotateBody.AddRelativeTorque(new Vector3(0f, -longitdudinalForce * r + motorTorque, 0f)); ;
+        wheelRotateTransform.localPosition = new Vector3(0f, 0f, 0f);
+
+        Debug.Log("r:" + r + "rdot:  " + rdot+  " fz:" + normalForce);
+        Debug.DrawRay(hubTransform.position, r * hubNormalVec, Color.red, .1f, false);
+    }
+
+    float FxModel(float slipRatio, float normalForce)
+    {
+        return fxSlope * normalForce * slipRatio;
+    }
+
+    float LongitudinalForce(Rigidbody wheel, RaycastHit hitinfo)
+    {
+        float omegaTire = hubTransform.InverseTransformDirection(wheelRotateBody.angularVelocity).y;
+        float contactPatchVx = omegaTire * r;
+        float hubVx = hubTransform.InverseTransformDirection(wheel.velocity).x;
+
+        Debug.Log("hubvx: " + hubVx + " contact patch vx: " + contactPatchVx + "slip ratio: " + slipRatio);
+        Debug.Log("fx:" + longitdudinalForce);
+
+        if (Mathf.Abs(hubVx) < .001) slipRatio = contactPatchVx;
         else
         {
-            Vector3 normalForce = new Vector3(0f, 0f, 0f);
-            wheelRigidBody.AddRelativeForce(normalForce);
-
+            slipRatio = contactPatchVx / hubVx - 1;
+            slipRatio = Mathf.Sign(slipRatio) * Mathf.Min(Mathf.Abs(slipRatio), .1f);
         }
-
-        wheelRotateTransform.localPosition = new Vector3(0f, 0f, 0f);
-        wheelRotateBody.angularVelocity = -wheelRotateTransform.up * wheelRigidBody.velocity.z / r0;
-        Debug.DrawRay(hubTransform.position, hubNormalVec, Color.red, .1f);
-        Debug.Log(hitinfo.distance);
-        Debug.DrawRay(hubTransform.position, hubTransform.forward*.5f, Color.blue, 3f);
-        Debug.Log(hitinfof.distance);
-
+        //return FxModel(slipRatio, normalForce);
+        return 0;
     }
-    
+
+
     [Tooltip("Unloaded Radius of Tire")]
-    public float r0 = 0.5f;
+    public float r0 = .5f;
     [Tooltip("Spring Constant of Tire N/m")]
-    public double k = 10;
+    public float k = -10f;
     [Tooltip("Damping Coefficient of Tire N-s/m")]
-    public double c = -.1;
-    private double r;
-    private double rdot;
+    public float c = -1f;
+    private float r;
+    private float rdot;
+    private float normalForce;
+    private float longitdudinalForce;
+    public float slipRatio;
+    public float motorTorque;
+    public float fxSlope;
     private Vector3 hubNormalVec;
+    public Vector3 newRot;
     private Transform hubTransform;
     private Rigidbody wheelRigidBody;
     private Rigidbody wheelRotateBody;
     private Transform wheelRotateTransform;
-
 }
